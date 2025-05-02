5cb960d8de9cb5a01dc8a3457c8672d63ff5980b
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2025-05-02 17:59:02 +0530

    fix3 - not working later check

--- portfolio_react/src/index.jsx
+++ portfolio_react/src/index.jsx
@@ -1,4 +1,14 @@
 import { createRoot } from "react-dom/client";
 import Skills from "./Skills";
+// document.addEventListener("DOMContentLoaded", () => {
+//     const rootElement = document.getElementById("react-skills");
+//     if (rootElement) {
+//       const root = createRoot(rootElement);
+//       root.render(<Skills />);
+//     } else {
+//       console.error("Could not find element with ID 'react-skills'");
+//     }
+//   });
+
 const root = createRoot(document.getElementById("react-skills"));
 root.render(<Skills />);
